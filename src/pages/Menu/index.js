78b5ab86d8cc2e63c201d82f9fe49dc0dbc75f9a
Ashokkumar <ashokkumarash96@gmail.com>
2023-06-28 07:53:53 +0530

    completed

---
+++ src/pages/Menu/index.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Tabs } from "../../components/Tabs";
+import {
+  fetchProducts,
+  selectAllProducts,
+} from "../../stores/menu/productsSlice";
+
+const Menu = () => {
+  const dispatch = useDispatch();
+  const productStatus = useSelector((state) => state.products.status);
+  const products = useSelector(selectAllProducts);
+
+  useEffect(() => {
+    if (productStatus === "idle") {
+      dispatch(fetchProducts());
+    }
+  }, [dispatch, productStatus]);
+
+  return <Tabs list={["breakfast", "brunch", "dinner"]} />;
+};
+
+export default Menu;
